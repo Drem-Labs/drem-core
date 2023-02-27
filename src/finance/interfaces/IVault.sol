a659f95a3e151a50d96599b586a1af9c1c146520
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2023-02-27 11:56:57 -0500

    vault interface

---
+++ src/finance/interfaces/IVault.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "../libraries/DataTypes.sol";
+
+interface IVault is IERC20 {
+
+    // steps and assets
+    function steps() external view returns(DataTypes.StepInfo[] memory);
+    function addSteps(DataTypes.StepInfo[] steps) external;
+    function addTrackedAsset(address asset) external;
+    function removeTrackedAsset(address asset) external;
+
+    // share accounting
+    function mintShares(uint256 shareAmount) external;
+    function burnShares(uint256 shareAmount) external;
+
+    // safegaurding funds
+    function withdraw(uint256 shareAmount, DataTypes.AssetExpectation[] expectations) external;
+}
---
+++ src/finance/libraries/DataTypes.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+library DataTypes {
+    // basic step routing information
+    struct StepInfo {
+        address interactionAddress,
+        bytes4 functionSelector
+    }
+
+    // user expectations for the withdrawal assets (can't check with oracles in worst-case)
+    // note: the amount is not being stored or used often, so best to keep it as a uint256 in case users have a ton of a bespoke token
+    struct AssetExpectation {
+        address assetAddress,
+        uint256 amount
+    }
+}

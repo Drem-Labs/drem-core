ba1e510eb087d07dc15f8eb33456ba09d2197959
tokyoDan67 <danielyamagata12@gmail.com>
2023-02-27 23:41:11 -0500

    Added to DremHub

--- src/finance/core/DremHub.sol
+++ src/finance/core/DremHub.sol
@@ -1,17 +1,52 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 
-contract DremHub {
+import {Ownable2StepUpgradeable} from "@openzeppelin-upgradeable/contracts/access/Ownable2StepUpgradeable.sol";
+import {Initializable} from "@openzeppelin-upgradeable/contracts/proxy/utils/Initializable.sol";
+import {UUPSUpgradeable} from "@openzeppelin-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol"; 
+import {IDremHub} from "../interfaces/IDremHub.sol";
 
-    // keccak256(contractAddress, functionSelector) => keccak256(encodedArgs) => bool
-    mapping(bytes32 => mapping(bytes32 => bool)) whitelistedStep;
+// Initializable is inherited from Ownable2StepUpgradeable
+contract DremHub is Ownable2StepUpgradeable, UUPSUpgradeable, IDremHub {
 
     // keccak256('DremHub.ANY_CALL')
     bytes32 private constant ANY_CALL = 0x6d1d2d8a4086e5e1886934ed17d0fea24fea45860e94b9c1d77a6a38407e239b;
 
+    // keccak256(contractAddress, functionSelector) => keccak256(encodedArgs) => bool
+    mapping(bytes32 => mapping(bytes32 => bool)) whitelistedStep;
+
     bool public isTradingAllowed;
 
-    function setGlobalTrading(bool _isTradingAllowed) external {}
+    /**
+     * @dev This empty reserved space is put in place to allow future versions to add new
+     * variables without shifting down storage in the inheritance chain.
+     * See https://docs.openzeppelin.com/contracts/4.x/upgradeable#storage_gaps
+     */
+    uint256[49] private __gap;
 
-    function addStep() external {}
+    constructor() {
+        _disableInitializers();
+    }
+
+    function init() external initializer {
+        __Ownable2Step_init();
+        // Technically unnecessary but good practice...
+        __UUPSUpgradeable_init();
+    }
+
+    function setGlobalTrading(bool _isTradingAllowed) external onlyOwner {
+        if (isTradingAllowed == _isTradingAllowed) revert InvalidParam();
+        isTradingAllowed = _isTradingAllowed;
+    }
+
+    // Need to verify with Drem team about global state
+    function setGlobalState() external onlyOwner {}
+
+    function addWhitelistedStep() external {}
+
+    function dremHubBeforeTransferHook() external view {
+        if(!(isTradingAllowed)) revert TradingDisabled();
+    }
+
+    function _authorizeUpgrade(address newImplementation) internal override onlyOwner {}
 }
--- src/finance/interfaces/IDremHub.sol
+++ src/finance/interfaces/IDremHub.sol
@@ -1,19 +1,18 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 
-contract DremHub {
-//     mapping(address => address) vaultToComptroller:  keeps track of the vault’s given comptroller
-// bytes32 ANY_CALL
-// keccak256(‘
-// mapping(bytes32 => bytes32 => bool) whitelistedStep
-// keccak256(contractAddress, functionSelector) => keccack256(encodedArgs) => bool
-// Get around function variability via ANY_CALL: 
-// If whitelistedStep[keccak256(contractAddress, functionSelector)] [ANY_CALL] is true, enable any encoded args
-// Else, whitelistedStep[keccak256(contractAddress, functionSelector)] [keccak256(encodedArgs)] must be true 
-// Allow for global trading variable
-// Will consider whether or not to have managers control transferability
+interface IDremHub {
 
-// keccak256(contractAddress, functionSelector) => keccak25
-mapping(bytes32 => mapping(bytes32 => bool)) whitelistedStep;
+    /**
+     *  'isTradingnAllowed' is set to false
+     */
+    error TradingDisabled();
+
+    /**
+     *  Invalid parameter passed into function
+     */
+    error InvalidParam();
+
+    function addWhitelistedStep() external;
 
 }

6947c81334073f765de9daae5d098fe0f7da5839
Naveen Ailawadi <naveen.ailawadi91@gmail.com>
2023-03-01 18:38:05 -0500

    made step and base step

--- src/finance/steps/steps/TransferStep.sol
+++ src/finance/steps/steps/TransferStep.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity =0.8.17;
 
-import BaseStep from "../BaseStep.sol";
+import {BaseStep} from "../BaseStep.sol";
 
 contract TransferStep is BaseStep {
 

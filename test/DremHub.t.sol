2985bac48d7de28db245078584821fff77894fdd
tokyoDan67 <danielyamagata12@gmail.com>
2023-03-02 22:45:45 -0500

    Wrote some PriceAggregator tests

--- test/DremHub.t.sol
+++ test/DremHub.t.sol
@@ -2,13 +2,13 @@
 pragma solidity =0.8.17;
 
 import "forge-std/Test.sol";
+import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
+import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import {DataTypes} from "../src/finance/libraries/DataTypes.sol";
 import {DremHub} from "../src/finance/core/DremHub.sol";
 import {Events} from "../src/finance/libraries/Events.sol";
 import {Helper} from "./reference/Helper.sol";
 import {IDremHub} from "../src/finance/interfaces/IDremHub.sol";
-import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import {ERC20} from "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract DremHubHelper is Test, Helper {
     DremHub dremHub;
